517136d78f9e8d10c86699c7d9563b14e14af1e9
AndreeCarrillo <u20211b589@upc.edu.pe>
2023-06-06 22:39:43 -0500

    create proyect

---
+++ src/main/java/com/example/discoverbackend/DiscoverBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.discoverbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoverBackendApplication.class, args);
+    }
+
+}
